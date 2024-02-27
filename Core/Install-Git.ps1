fa206d15d7e671552ccbe29c88ef070a86a6a99b
IGL-Nick <159208587+IGL-Nick@users.noreply.github.com>
2024-02-27 15:55:32 -0500

    Update Install-Git.ps1

--- Core/Install-Git.ps1
+++ Core/Install-Git.ps1
@@ -1,30 +1,24 @@
 $workingDirectory = 'c:\drop\GitInstall'
-try{
-    $statusGit = git version
-    Write-Host 'Git is installed'
+
+Try{mkdir $workingDirectory
+    Write-Host 'Creating working directory'
 }
 catch{
-    Write-Host 'Git is not installed. Installing...'
-    
-    Try{mkdir $workingDirectory
-        Write-Host 'Creating working directory'
-    }
-    catch{
-        Write-Host 'Working directory exists'
-    }
+    Write-Host 'Working directory exists'
+}
 
-    # get latest download url for git-for-windows 64-bit exe
-    $git_url = "https://api.github.com/repos/git-for-windows/git/releases/latest"
+# get latest download url for git-for-windows 64-bit exe
+$git_url = "https://api.github.com/repos/git-for-windows/git/releases/latest"
 
 
-    $asset = Invoke-RestMethod -Method Get -Uri $git_url | % assets | where name -like "*64-bit.exe"
-    # download installer
-    $installer = "$workingDirectory\$($asset.name)"
-    Invoke-WebRequest -Uri $asset.browser_download_url -OutFile $installer
-    
-    $git_install_inf = "$workingDirectory\git_install.inf"
-    
-    New-Item $git_install_inf -ItemType File -force -Value "[Setup]
+$asset = Invoke-RestMethod -Method Get -Uri $git_url | % assets | where name -like "*64-bit.exe"
+# download installer
+$installer = "$workingDirectory\$($asset.name)"
+Invoke-WebRequest -Uri $asset.browser_download_url -OutFile $installer
+
+$git_install_inf = "$workingDirectory\git_install.inf"
+
+New-Item $git_install_inf -ItemType File -force -Value "[Setup]
 Lang=default
 Dir=C:\Program Files\Git
 Group=Git
@@ -45,10 +39,9 @@ UseCredentialManager=Enabled
 EnableSymlinks=Disabled
 EnableBuiltinInteractiveAdd=Disabled"
   
-    # run installer
-       
-    $install_args = "/SP- /VERYSILENT /SUPPRESSMSGBOXES /NOCANCEL /NORESTART /CLOSEAPPLICATIONS /RESTARTAPPLICATIONS /LOADINF=""$git_install_inf"""
-    Start-Process -FilePath $installer -ArgumentList $install_args -Wait
+# run installer
+   
+$install_args = "/SP- /VERYSILENT /SUPPRESSMSGBOXES /NOCANCEL /NORESTART /CLOSEAPPLICATIONS /RESTARTAPPLICATIONS /LOADINF=""$git_install_inf"""
+Start-Process -FilePath $installer -ArgumentList $install_args -Wait
 
 
-}
