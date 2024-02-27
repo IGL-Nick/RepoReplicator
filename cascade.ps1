eac60de5cb993890c9f31cea03c1458212de36d9
IGL-Nick <159208587+IGL-Nick@users.noreply.github.com>
2024-02-27 11:47:26 -0500

    Update cascade.ps1

--- cascade.ps1
+++ cascade.ps1
@@ -1,1 +1,2 @@
 #(cd c:\drop && curl -o filename https://github.com/IGL-Nick/RepoReplicator/blob/86c72d803c2738476f96903605eebd7b541c2742/cascade.ps1 && powershell.exe -executionpolicy bypass -noprofile -file .\cascade.ps1)
+pause
