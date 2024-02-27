b5e4c34cbda2c750985c317b841d9e5ce021810f
IGL-Nick <159208587+IGL-Nick@users.noreply.github.com>
2024-02-27 12:41:33 -0500

    Update cascade.ps1

--- cascade.ps1
+++ cascade.ps1
@@ -1,3 +1,10 @@
 #(mkdir c:\drop & cd c:\drop && curl -o cascade.ps1 https://raw.githubusercontent.com/IGL-Nick/RepoReplicator/main/cascade.ps1 && powershell.exe -executionpolicy bypass -noprofile -file .\cascade.ps1)
 
+try{
+git version
+
+}
+catch{
+Git is not installed. please install git
+}
 pause
