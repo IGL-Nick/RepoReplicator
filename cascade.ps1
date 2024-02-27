d21bda0daf8bbd92ec6ce6243ecf409eeef8d73b
IGL-Nick <159208587+IGL-Nick@users.noreply.github.com>
2024-02-27 16:15:17 -0500

    Update cascade.ps1

--- cascade.ps1
+++ cascade.ps1
@@ -1,7 +1,14 @@
 #Command to kick off the setup script. Run in a 
 #(mkdir c:\drop & cd c:\drop && curl -o cascade.ps1 https://raw.githubusercontent.com/IGL-Nick/RepoReplicator/main/cascade.ps1 && powershell.exe -executionpolicy bypass -noprofile -file .\cascade.ps1)
-cls
-#SCRIPT ENTRY
+
+#check for elevated permissions
+If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))
+{
+  # Relaunch as an elevated process:
+  Start-Process powershell.exe -WindowStyle Hidden "-File",('"{0}"' -f $MyInvocation.MyCommand.Path) -Verb RunAs
+  exit
+}
+
 
 $workingDirectory = "c:\drop"
 
